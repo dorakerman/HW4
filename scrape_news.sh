2d50e91f2948f54a4a10deb3c8a618294b43bfa2
Dor Akerman <dor5894@gmail.com>
2020-12-05 19:36:43 +0200

    First version - 05/12/2020

---
+++ scrape_news.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Starting..."
+wget https://www.ynetnews.com/category/3082
+#Use grep and sort to get all the links and sort them to appear only once.
+#Store links in a different file.
+grep -o -E '(https://www.ynetnews.com/article/)([a-z]|[0-9]|[A-Z]){9}(")+' 3082\
+	| sort -u | sed 's/"//'> list_of_pages.txt
+#Count and store number of lines in result file.
+wc -l list_of_pages.txt | sed 's/list_of_pages.txt//' > results.csv
+#Each line is a link, the loop goes over all of them. 
+while read line
+do
+	wget -O temp_content.txt $line
+	bibi_num=$(grep -o 'Netanyahu' temp_content.txt | wc -l)
+	gantz_num=$(grep -o 'Gantz' temp_content.txt | wc -l)
+	#If both names do not appear, insert - sign.
+	if [ "$bibi_num" -eq 0 ] && [ "$gantz_num" -eq 0 ]; then
+		echo "${line}, -">>results.csv
+	else
+		echo "${line}, Netanyahu, ${bibi_num}, Gantz, ${gantz_num}">>results.csv
+	fi
+done<list_of_pages.txt
